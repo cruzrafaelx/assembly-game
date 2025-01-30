237f107f7d0aec9bce660c0072b3772a4ff3176a
cruzrafaelx <rafaelcruz@outlook.de>
2025-01-30 05:59:03 +0100

    Add function that checks if letter is correct or wrong

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import clsx from 'clsx';
 import { useState } from 'react';
 import { languages } from './languages';
 import Language from './Language';
@@ -6,6 +7,10 @@ import Language from './Language';
 function App() {
 
   const [currentWord, setCurrentWord] = useState("React")
+  const [chosenLetter, setChosenLetter] = useState([])
+  const [correctLetter, setCorrectLetter] = useState({})
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
   const languageList = languages.map(lang =>
     <Language name={lang.name} 
@@ -19,11 +24,55 @@ function App() {
     return <span key={index}>{upperCase}</span>
   })
 
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
   const alphabetList = alphabet.split("").map(letter => (
-    <button key={letter}>{letter.toUpperCase()}</button>
+    <button 
+      onClick={()=> {handleChosenLetter(letter); checkLetter(letter)}}
+      className={clsx(
+        'btn',
+        correctLetter[letter] === true && 'correct-letter',
+        correctLetter[letter] === false && 'wrong-letter'
+      )}
+      key={letter}
+    >{letter.toUpperCase()}</button>
   ))
+
+  //Functions
+  
+  //This function adds the clicked letter to the chosenLetter state
+  function handleChosenLetter(letter){
+    setChosenLetter(prevChosenLetter => {
+      
+      return prevChosenLetter.includes(letter) ? prevChosenLetter : [...prevChosenLetter, letter] 
+
+      })
+  
+      //You can also use Sets to solve this
+      // const lettersSet = new Set(prevLetters)
+      // lettersSet.add(letter)
+      // return Array.from(lettersSet)
+  }
+
+  //This function checks if the clicked letter is included in the current word, it consequently changes the isCorrect state as well. 
+  function checkLetter(letter){
+    const result = currentWord.toLowerCase().split("").includes(letter)
+    console.log("checking letter:", letter, "Result:", result)
+
+    setCorrectLetter(prevCorrectLetter => {
+
+      const updatedCorrectLetter = {
+        ...prevCorrectLetter,
+        [letter]: result
+      }
+
+  
+      return updatedCorrectLetter
+     
+    })
+  }
+  
+  console.log("Chosen letter: ", chosenLetter)
+  console.log("Correct letter: ", correctLetter)
+  
 
   return (
     <main>
